508903ee404e81926aac3dcfb1154f0f6a212459
MohamedAlDeep <mohamedsamir22446688@gmail.com>
2023-06-30 19:36:57 +0300

    finished the repos fetching

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,14 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent{
+  configUrl = '';
+  repos: any;
 
-  constructor(private router: Router ) {
+  constructor(private http: HttpClient ) {
 
+  }
 
-   }
+  getConfig() {
+  return this.http.get("https://api.github.com/users/MohamedAlDeep/repos");
+  }
+  showConfig() {
+    this.getConfig()
+      .subscribe((data) => {
+        this.repos = data
+        })
+    }
+
+  ngOnInit(){
+    this.showConfig()
+  }
+
 }
